b92e4beb6974d891977bec24e566031b8c14d40b
kgni <admin@mdia.dk>
2022-03-21 20:15:27 +0100

    add challenge March 21, 2022

---
+++ 8 kyu/Is it a palindrome.js
@@ -0,0 +1,11 @@
+// March 21, 2022
+
+/*
+Write a function that checks if a given string (case insensitive) is a palindrome.
+*/
+
+function isPalindrome(x) {
+	x = x.toLowerCase();
+
+	return x === x.split('').reverse().join('') ? true : false;
+}
