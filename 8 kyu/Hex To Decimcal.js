782ef3f17dcbf8e2924a1788e8650de2541ea8f8
kgni <admin@mdia.dk>
2022-03-24 10:49:25 +0100

    add challenge March 24, 2022

---
+++ 8 kyu/Hex To Decimcal.js
@@ -0,0 +1,9 @@
+// March 24, 2022
+
+/*
+Complete the function which converts hex number (given as a string) to a decimal number.
+*/
+
+function hexToDec(hexString) {
+	return parseInt(hexString, 16);
+}
