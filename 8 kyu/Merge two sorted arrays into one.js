f259ace28de3f20e2f8ef1beaead275a49e5725c
kgni <admin@mdia.dk>
2022-03-25 18:58:58 +0100

    edit typo

--- 8 kyu/Merge two sorted arrays into one.js
+++ 8 kyu/Merge two sorted arrays into one.js
@@ -17,7 +17,7 @@ Happy coding!
 */
 
 function mergeArrays(arr1, arr2) {
-	if (arr1.length === 0 && arr2.legnth === 0) {
+	if (arr1.length === 0 && arr2.length === 0) {
 		return [];
 	}
 	return Array.from(new Set(arr1.concat(arr2).sort((a, b) => a - b)));
