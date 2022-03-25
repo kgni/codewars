93b7cb61088edd64b83115681733f4db9bb5c820
kgni <admin@mdia.dk>
2022-03-25 19:24:33 +0100

    add challenge March 25, 2022

---
+++ 8 kyu/Well of Ideas - Easy Version.js
@@ -0,0 +1,25 @@
+// March 25, 2022
+
+/*
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+*/
+
+function well(x) {
+	let goodIdeas = x.reduce((ideas, idea) => {
+		if (idea === 'good') {
+			return ++ideas;
+		} else {
+			return ideas + 0;
+		}
+	}, 0);
+
+	if (goodIdeas > 2) {
+		return 'I smell a series!';
+	} else if (goodIdeas === 2 || goodIdeas === 1) {
+		return 'Publish!';
+	} else {
+		return 'Fail!';
+	}
+}
