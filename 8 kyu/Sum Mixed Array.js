d849c708bad177ccfa1e1fc6a91ec211bb97c2a7
kgni <admin@mdia.dk>
2022-03-30 21:42:48 +0200

    add challenge March 30, 2022

---
+++ 8 kyu/Sum Mixed Array.js
@@ -0,0 +1,11 @@
+// March 30, 2022
+
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+function sumMix(x) {
+	return x.reduce((acc, c) => acc + Number(c), 0);
+}
