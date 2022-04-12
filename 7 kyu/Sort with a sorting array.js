43bdf26a57cca06b6fb2f990f779e244416231a6
mdiadk <acc@mdia.dk>
2022-04-12 20:31:39 +0200

    add challenge April 12, 2022

---
+++ 7 kyu/Sort with a sorting array.js
@@ -0,0 +1,20 @@
+// April 12, 2022
+
+/*
+Sort an array according to the indices in another array. It is guaranteed that the two arrays have the same size, and that the sorting array has all the required indices.
+
+initialArray = ['x', 'y', 'z'] sortingArray = [ 1, 2, 0]
+
+sort(initialArray, sortingArray) => ['z', 'x', 'y']
+
+sort(['z', 'x', 'y'], [0, 2, 1]) => ['z', 'y', 'x']
+*/
+
+function sort(initialArray, sortingArray) {
+	let n = initialArray.length;
+	let result = [];
+	for (let i = 0; i < n; i++) {
+		result[sortingArray[i]] = initialArray[i];
+	}
+	return result;
+}
