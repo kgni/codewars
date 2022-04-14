dd4bedd2b8e120f0734d03341fbc5043ccbd2133
kgni <admin@mdia.dk>
2022-04-14 09:02:21 +0200

    add challenge April 14, 2022

---
+++ 8 kyu/Name Shuffler.js
@@ -0,0 +1,10 @@
+// April 14, 2022
+
+/*
+Write a function that returns a string in which firstname is swapped with last name.
+
+nameShuffler('john McClane'); => "McClane john"
+*/
+function nameShuffler(str) {
+	return str.split(' ').reverse().join(' ');
+}
