2e4d85f6411a37b4b1b1bde6e4af810955b2cfee
kgni <admin@mdia.dk>
2022-04-17 10:37:48 +0200

    add challenge April 17, 2022

---
+++ 8 kyu/If you can't sleep, just count sheep.js
@@ -0,0 +1,24 @@
+// April 17, 2022
+
+/*
+If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+*/
+let countSheep = function (num) {
+	if (num === 0) {
+		return '';
+	}
+
+	let sheepString = '';
+	for (let i = 1; i <= num; i++) {
+		sheepString += `${i} sheep...`;
+	}
+	return sheepString;
+};
+
+// Can also be done by creating an array with as many elements as the number we input:
+
+const countSheep = (n) =>
+	[...Array(n)].map((el, index) => `${index + 1} sheep...`).join('');
