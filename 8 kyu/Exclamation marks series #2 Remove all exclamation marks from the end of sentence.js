102566312d62053d733c93c3271ed6b056f0eecb
kgni <admin@mdia.dk>
2022-04-24 10:56:10 +0200

    add challenge April 24, 2022

---
+++ 8 kyu/Exclamation marks series #2 Remove all exclamation marks from the end of sentence.js
@@ -0,0 +1,28 @@
+// April 24, 2022
+
+/*
+Description:
+Remove all exclamation marks from the end of sentence.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
+
+function remove(string) {
+	while (string && string.slice(-1) == '!') {
+		string = string.slice(0, -1);
+	}
+	return string;
+}
+
+function remove(string) {
+	while (string[string.length - 1] === '!') {
+		string = string.substring(0, string.length - 1);
+	}
+	return string;
+}
