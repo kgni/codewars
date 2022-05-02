1bf56dc33d750a87ed2f5f7f546e69da0f6f35b4
kgni <admin@mdia.dk>
2022-05-02 08:28:18 +0200

    add challenge May 02, 2022

---
+++ 8 kyu/Convert a Boolean to a String.js
@@ -0,0 +1,10 @@
+// May 02, 2022
+
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+function booleanToString(b) {
+	return String(b);
+}
