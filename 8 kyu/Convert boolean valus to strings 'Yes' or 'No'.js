69986d734deb6129c2cf3c51adb439ed4bd9264e
kgni <admin@mdia.dk>
2022-05-02 08:30:23 +0200

    add challenge May 02, 2022

---
+++ 8 kyu/Convert boolean valus to strings 'Yes' or 'No'.js
@@ -0,0 +1,8 @@
+// May 02, 2022
+
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+function boolToWord(bool) {
+	return bool ? 'Yes' : 'No';
+}
