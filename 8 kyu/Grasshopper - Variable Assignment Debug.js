d98718ca7cf88ce434751b6fd34490078ba2162d
mdiadk <acc@mdia.dk>
2022-05-04 12:52:15 +0200

    add challenge May 04, 2022

---
+++ 8 kyu/Grasshopper - Variable Assignment Debug.js
@@ -0,0 +1,11 @@
+// May 04, 2022
+
+/*
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+*/
+
+const a = 'dev';
+const b = 'Lab';
+
+const name = a + b;
