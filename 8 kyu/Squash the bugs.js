f7d05898e923cddcbe0d364ad9fb1075fbe8fca6
mdiadk <acc@mdia.dk>
2022-05-05 14:28:27 +0200

    add challenge May 05, 2022

---
+++ 8 kyu/Squash the bugs.js
@@ -0,0 +1,19 @@
+// May 05, 2022
+
+/*
+Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+
+There will only be one 'longest' word.
+*/
+
+function findLongest(str) {
+	let words = str.split(' ');
+	let length = 0;
+
+	for (let i = 0; i < words.length; i++) {
+		if (words[i].length > length) {
+			length = words[i].length;
+		}
+	}
+	return length;
+}
