bfbd0eaf07d4001af35b5f686da93ea67fd11eaf
kgni <admin@mdia.dk>
2022-05-06 16:58:01 +0200

    add challenge May 06, 2022

--- 8 kyu/Kata Example Twist.js
+++ 8 kyu/Kata Example Twist.js
@@ -8,8 +8,17 @@ Add the value "codewars" to the array websites/Websites 1,000 times.
 var websites = []
 */
 // add the value "codewars" to the websites array 1,000 times
-const websites = [];
+let websites = [];
 
 for (let i = 0; i < 1000; i++) {
 	websites.push('codewars');
 }
+
+// could also be done by using new array and fill:
+
+let websites2 = new Array(1000).fill('codewars');
+
+// done with while loop
+
+let websites3 = [];
+while (websites3.length < 1000) websites3.push('codewars');
