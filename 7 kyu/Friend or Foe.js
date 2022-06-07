7c0cc0d0d9c6c17277d61a3a53ff3002c29794ef
mdiadk <acc@mdia.dk>
2022-06-07 12:21:45 +0200

    add challenge June 07, 2022

---
+++ 7 kyu/Friend or Foe.js
@@ -0,0 +1,27 @@
+// June 07, 2022
+
+/*
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+Note: keep the original order of the names in the output.
+*/
+
+//PREP
+
+//P - Array of strings
+//R - Return new array containing only the strings that has a length of 4
+
+function friend(friends) {
+	// filter through array, return only the elements that has a length of 4
+
+	return friends.filter((friend) => friend.length === 4);
+}
+
+//E -
