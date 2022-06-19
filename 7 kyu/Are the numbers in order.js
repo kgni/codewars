2572ee6867e03f64430ae4886d80ad9caed02e21
kgni <admin@mdia.dk>
2022-06-19 12:01:14 +0200

    add challenge June 19, 2022

---
+++ 7 kyu/Are the numbers in order.js
@@ -0,0 +1,40 @@
+// June 19, 2022
+
+/*
+Are the numbers in order?
+In this Kata, your function receives an array of integers as input. Your task is to determine whether the numbers are in ascending order. An array is said to be in ascending order if there are no two adjacent integers where the left integer exceeds the right integer in value.
+
+For the purposes of this Kata, you may assume that all inputs are valid, i.e. non-empty arrays containing only integers.
+
+Note that an array of 1 integer is automatically considered to be sorted in ascending order since all (zero) adjacent pairs of integers satisfy the condition that the left integer does not exceed the right integer in value. An empty list is considered a degenerate case and therefore will not be tested in this Kata - feel free to raise an Issue if you see such a list being tested.
+
+For example:
+
+inAscOrder([1,2,4,7,19]); // returns true
+inAscOrder([1,2,3,4,5]); // returns true
+inAscOrder([1,6,10,18,2,4,20]); // returns false
+inAscOrder([9,8,7,6,5,4,3,2,1]); // returns false because the numbers are in DESCENDING order
+N.B. If your solution passes all fixed tests but fails at the random tests, make sure you aren't mutating the input array.
+*/
+
+//PREP
+
+//P - array of integers
+//R - boolean (true if all items in the array is in ascending order, false if not)
+
+function inAscOrder(arr) {
+	// loop through array, if the current element is greater than the next, return false
+	for (let i = 0; i < arr.length - 1; i++) {
+		if (arr[i] > arr[i + 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+//E -
+
+console.log(inAscOrder([1, 2, 4, 7, 19]));
+
+// doing it with join and sort
+
+const inAscOrder = (arr) => arr.join('') === arr.sort((a, b) => a - b).join('');
