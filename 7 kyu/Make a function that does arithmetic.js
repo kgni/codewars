8726e3f9d74a963027194266f7c06eba70b51431
kgni <admin@mdia.dk>
2022-06-19 11:02:17 +0200

    add challenge June 19, 2022

---
+++ 7 kyu/Make a function that does arithmetic.js
@@ -0,0 +1,37 @@
+// June 19, 2022
+
+/*
+Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+The four operators are "add", "subtract", "divide", "multiply".
+
+A few examples:(Input1, Input2, Input3 --> Output)
+
+5, 2, "add"      --> 7
+5, 2, "subtract" --> 3
+5, 2, "multiply" --> 10
+5, 2, "divide"   --> 2.5
+Try to do it without using if statements!
+*/
+
+//PREP
+
+//P - p1 number, p2 number, p3 string (can be 1 of 4 arithmetic operators as a string)
+//R - integer with the arithmetic performed on p1 and p2.
+function arithmetic(a, b, operator) {
+	// create switch statement
+
+	switch (operator) {
+		case 'add':
+			return a + b;
+		case 'subtract':
+			return a - b;
+		case 'multiply':
+			return a * b;
+		case 'divide':
+			return a / b;
+	}
+}
+//E -
