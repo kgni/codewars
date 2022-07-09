5d83d4c5b36fdce1341cb5a3f70ed8b1533b97f1
kgni <admin@mdia.dk>
2022-07-09 10:37:37 +0200

    add challenge July 09, 2022

---
+++ 7 kyu/Sort arrays - 1.js
@@ -0,0 +1,16 @@
+// July 09, 2022
+
+/*
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+*/
+
+//PREP
+
+//P - array
+//R - return array sorted in lexicographical order (alphabetical order)
+//E -
+
+sortme = function (names) {
+	// by default the sort() array method sorts in lexicographical order.
+	return names.sort();
+};
