01d2780f8d5f7f67455d3ba2dea6dd52d6c358e7
kgni <admin@mdia.dk>
2022-07-14 12:41:59 +0200

    add challenge July 14, 2022

---
+++ 8 kyu/Find Nearest square number.js
@@ -0,0 +1,14 @@
+// July 14, 2022
+
+/*
+Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+*/
+
+//PREP
+
+//P -
+//R -
+function nearestSq(n) {
+	return Math.pow(Math.round(Math.sqrt(n)), 2);
+}
+//E -
