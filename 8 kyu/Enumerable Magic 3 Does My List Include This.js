b9d9fd839a94bc04c30a00a4b8ca14b97a6e3eea
kgni <admin@mdia.dk>
2022-07-17 10:45:42 +0200

    add challenge July 17, 2022

---
+++ 8 kyu/Enumerable Magic 3 Does My List Include This.js
@@ -0,0 +1,10 @@
+// July 17, 2022
+
+/*
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+
+function include(arr, item) {
+	// loop through array and see if it includes item
+	return arr.includes(item);
+}
