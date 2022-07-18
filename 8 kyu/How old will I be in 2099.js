b031e8a7e77b37579f8154d45c3b1848da91abea
kgni <admin@mdia.dk>
2022-07-18 11:40:13 +0200

    add challenge July 18, 2022

---
+++ 8 kyu/How old will I be in 2099.js
@@ -0,0 +1,30 @@
+// July 18, 2022
+
+/*
+Philip's just turned four and he wants to know how old he will be in various years in the future such as 2090 or 3044. His parents can't keep up calculating this so they've begged you to help them out by writing a programme that can answer Philip's endless questions.
+
+Your task is to write a function that takes two parameters: the year of birth and the year to count years in relation to. As Philip is getting more curious every day he may soon want to know how many years it was until he would be born, so your function needs to work with both dates in the future and in the past.
+
+Provide output in this format: For dates in the future: "You are ... year(s) old." For dates in the past: "You will be born in ... year(s)." If the year of birth equals the year requested return: "You were born this very year!"
+
+"..." are to be replaced by the number, followed and proceeded by a single space. Mind that you need to account for both "year" and "years", depending on the result.
+
+Good Luck!
+*/
+
+//PREP
+
+//P - p1 - integer, p2 - integer
+//R - String saying how old you are, when you will be born or if you were born that year
+
+function calculateAge(bornYear, futureYear) {
+	if (bornYear === futureYear) return 'You were born this very year!';
+	if (futureYear - bornYear === 1) return 'You are 1 year old.';
+	if (bornYear - futureYear === 1) return 'You will be born in 1 year.';
+	if (bornYear < futureYear)
+		return `You are ${futureYear - bornYear} years old.`;
+
+	if (bornYear > futureYear)
+		return `You will be born in ${bornYear - futureYear} years.`;
+}
+//E -
