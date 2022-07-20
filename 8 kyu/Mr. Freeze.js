341aeae8c7093e892364fc4253c18fffb1f293a3
kgni <admin@mdia.dk>
2022-07-20 12:21:20 +0200

    add challenge July 20, 2022

---
+++ 8 kyu/Mr. Freeze.js
@@ -0,0 +1,9 @@
+// July 20, 2022
+
+/*
+There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+*/
+
+//PREP
+
+Object.freeze(MrFreeze);
