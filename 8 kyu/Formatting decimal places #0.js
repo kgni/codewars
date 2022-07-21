d5a0f756bb18c2ef9588715e5c0ec83e17a1b9f3
kgni <admin@mdia.dk>
2022-07-21 09:33:21 +0200

    add challenge July 21, 2022

---
+++ 8 kyu/Formatting decimal places #0.js
@@ -0,0 +1,19 @@
+// July 21, 2022
+
+/*
+Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Example:    
+5.5589 is rounded 5.56   
+3.3424 is rounded 3.34
+*/
+
+//PREP
+
+//P - floated number
+//R - floated number rounded to two decimal places
+
+function twoDecimalPlaces(n) {
+	return Number(n.toFixed(2));
+}
+//E -
