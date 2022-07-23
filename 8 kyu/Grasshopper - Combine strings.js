dc489dd5e61d96f33ff1bcba7d9fb738f11a70ad
kgni <admin@mdia.dk>
2022-07-23 20:59:25 +0200

    add challenge July 23, 2022

---
+++ 8 kyu/Grasshopper - Combine strings.js
@@ -0,0 +1,23 @@
+// July 23, 2022
+
+/*
+Combine strings function
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+*/
+
+//PREP
+
+//P -
+//R -
+
+function combineNames(firstName, lastName) {
+	return `${firstName} ${lastName}`;
+}
+//E -
